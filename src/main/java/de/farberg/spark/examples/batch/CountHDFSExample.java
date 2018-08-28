462550c397be854c2726dbba6e0f1776fda4794e
Dennis <github@farberg.de>
2018-08-28 15:00:19 +0200

    Removed some unnecessary lines

--- src/main/java/de/farberg/spark/examples/batch/CountHDFSExample.java
+++ src/main/java/de/farberg/spark/examples/batch/CountHDFSExample.java
@@ -16,14 +16,12 @@ public class CountHDFSExample {
 				
 		JavaSparkContext sc = new JavaSparkContext(conf);
 
-		//JavaRDD<String> sentences = sc.textFile("webhdfs://141.72.191.110:30704/daten/yarn.cmd");
 		JavaRDD<String> sentences = sc.textFile("hdfs://hadoop-test-hadoop-hdfs-nn:9000/daten/yarn.cmd");
 		JavaRDD<String> words = sentences.flatMap(sentence -> Lists.newArrayList(sentence.split(" ")).iterator());
 		JavaPairRDD<String, Integer> wordMap = words.mapToPair(word -> new Tuple2<>(word, 1));
 		JavaPairRDD<String, Integer> reduceByKey = wordMap.reduceByKey((a, b) -> a + b);
 
 		
-		//System.out.println(reduceByKey.collect());
 		reduceByKey.saveAsTextFile("hdfs://hadoop-test-hadoop-hdfs-nn:9000/daten/demo.spark-out");
 		
 		sc.close();
